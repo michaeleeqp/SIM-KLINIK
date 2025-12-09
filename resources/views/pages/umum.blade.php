f5373e392291df0055e5ef5d8a0417b54cd7a2c6
putrinurfianiz-cmyk <putrinurfianiz@gmail.com>
2025-12-09 23:48:02 +0700

    perbaiki tekanan darah di ranap dan umum

--- resources/views/pages/umum.blade.php
+++ resources/views/pages/umum.blade.php
@@ -187,7 +187,10 @@
                                 <div class="col-12 col-md-6 col-lg-3">
                                     <div class="form-group">
                                         <label for="tekanan_darah">Tekanan Darah (mm/Hg)</label>
-                                        <input type="number" class="form-control" id="tekanan_darah" name="tekanan_darah" placeholder="120/80" required>
+                                        <input type="text" class="form-control" id="tekanan_darah" 
+       name="tekanan_darah" placeholder="120/80" 
+       required
+       oninput="this.value = this.value.replace(/[^0-9\/]/g, '')">
                                     </div>
                                 </div>
 
